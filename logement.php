c50f5bc98a235edc853e7ffe734007b5911a55c7
fanetech <louguefranck.20@gmail.com>
2022-04-12 18:07:55 +0000

    test

--- logement.php
+++ logement.php
@@ -1,3 +1,8 @@
+<?php
+$response = file_get_contents("https://kimobile.yentech.io/items/logement");
+$jsonClass = json_decode($response);
+$maison = $jsonClass->data;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +11,19 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<h3>Site des logements</h3>
+
 </head>
 
 <body>
-
+	<h3>Site des logements</h3>
+	<?php
+	foreach ($maison as $m) {
+		echo "<b>" . $m->titre . "</b><br>";
+		echo "<img width=180 src='https://kimobile.yentech.io/assets/" . $m->image . "'";
+		echo "prix : " . $m->prix . "<br>";
+		echo "superfice :" . $m->superficie . "<br>";
+	}
+	?>
 </body>
 
 </html>
